a773f323c6198fe71136d58b75a57eaea424c7a8
spreered <spreered@gmail.com>
2018-03-09 10:17:22 +0800

    add routes: missoin#index and admin::mission#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "missoins#index"
+
+  resource :missions, only: [:index]
+
+  namespace :admin do 
+    root "missions#index"
+    resource :missions
+  end
+
 end
