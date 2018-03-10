e46aae3a399de874099854af2b1e16e8a932d290
eggyy1224 <eggyy1224@gmail.com>
2018-03-10 19:46:14 +0800

    add users controller and show view, set image uploader gems to Gemfile, set up uploading environmentas

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  mount_uploader :avatar, AvatarUploader
   def admin?
     self.role=='admin'
   end
