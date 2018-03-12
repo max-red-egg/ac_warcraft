7cd8de504d8a2163ffa6d1c2a9e2494959dfd6df
spreered <spreered@gmail.com>
2018-03-12 23:44:09 +0800

    modify instance model state default value to teaming

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_12_123302) do
+ActiveRecord::Schema.define(version: 2018_03_12_152457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2018_03_12_123302) do
 
   create_table "instances", force: :cascade do |t|
     t.integer "mission_id"
-    t.string "state"
+    t.string "state", default: "teaming", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
