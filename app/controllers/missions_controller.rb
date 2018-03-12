faa74827e7ebfd5f1af514f1d1db7571b15b4ba3
spreered <spreered@gmail.com>
2018-03-13 00:06:25 +0800

    add challenge button, varify if user can challenge the mission

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -9,17 +9,16 @@ class MissionsController < ApplicationController
 
   def challenge
     #挑戰一個任務
-    
-    if !current_user.busy?
-      #如果使用者不是busy
+    mission = Mission.find(params[:id])
+    if current_user.take_mission?(mission)
+      #如果user可以挑戰這個任務
       #產生一個新副本
       instance = current_user.instances.build(mission_id: params[:id])
       instance.save
       flash[:notice] = "挑戰任務成功" 
       redirect_back(fallback_location: root_path)
     else
-      #使用者有任務進行中(user state is busy)
-      flash[:alert] = "已經有任務進行中，不能再挑戰新任務！"
+      flash[:alert] = "無法挑戰該任務"
       redirect_back(fallback_location: root_path)
     end
   end
