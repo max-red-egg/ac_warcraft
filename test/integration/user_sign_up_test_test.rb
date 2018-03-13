a8e28dd2723bb2bb5068f1b3411b37e48348fc8d
eggyy1224 <eggyy1224@gmail.com>
2018-03-13 08:41:58 +0800

    for merge

---
+++ test/integration/user_sign_up_test_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class UserSignUpTestTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  # test "new user can sign up" do
+  #   get new_user_registration_path
+
+  #   assert_difference 'User.count', 1 do
+      
+  #     post user_registration_path, params: { user: { email: "user@example.com",
+  #                                        password:              "password",
+  #                                        password_confirmation: "password" } }
+                                                     
+  #   end 
+  #   assert_response :redirect
+  #   follow_redirect!
+  #   assert_template 'missions/index'
+  # end
+end
