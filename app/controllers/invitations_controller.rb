e97b900dae75b305e5c6612a09af2f478f65e4e8
spreered <spreered@gmail.com>
2018-03-15 22:36:28 +0800

    add accept controller

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -7,6 +7,16 @@ class InvitationsController < ApplicationController
   end
 
   def accept
+    #要先做檢覈
+    # 1.是不是本人
+    # 2.該副本是否已經啟動
+    # 先欠著，要上trello
+
+    @invitation = Invitation.find(params[:id])
+    @invitation.state = 'accept'
+    @invitation.save
+    flash[:notice] = '接受任務'
+    redirect_to instance_path(@invitation.instance)
   end
 
   def decline
