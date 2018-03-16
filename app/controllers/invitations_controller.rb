19d421249ca2fd6d95b3d3d5ca4220b989556915
spreered <spreered@gmail.com>
2018-03-16 23:06:43 +0800

    invitations_controller: before_action set_invitation to dry the code

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,9 +1,10 @@
 class InvitationsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_invitation
 
   def show
   #要驗證只有受邀請的人可以進這個action
-    @invitation = Invitation.find(params[:id])
+
     @inviter = @invitation.user                    #邀請者
     @invitee = @invitation.invitee                 #受邀者
     @mission = @invitation.instance.mission
@@ -18,8 +19,6 @@ class InvitationsController < ApplicationController
     # 1.是不是本人
     # 2.該副本是否已經啟動
     # binding.pry
-
-    @invitation = Invitation.find(params[:id])
     if @invitation.invitee == current_user && @invitation.instance.state == 'teaming' && @invitation.state == 'inviting'
       @invitation.state = 'accept'
       @invitation.save
@@ -41,7 +40,6 @@ class InvitationsController < ApplicationController
     #要先做檢覈
     # 1.是不是本人
     # 2.invitation 狀態是 inviting
-    @invitation = Invitation.find(params[:id])
     if current_user == @invitation.invitee && @invitation.state == 'inviting'
       @invitation.state = 'decline'
       @invitation.save
@@ -56,9 +54,7 @@ class InvitationsController < ApplicationController
   def cancel
     #邀請者可以取消邀請
     #invitatoin.state = 'cancel'
-    @invitation = Invitation.find(params[:id])
     #只有發起這個邀請的人可以取消邀請
-
     if current_user == @invitation.user && @invitation.state == "inviting"
       @invitation.update!(state: "cancel")
       flash[:notice] = "取消邀請"
@@ -67,6 +63,11 @@ class InvitationsController < ApplicationController
       flash[:alert] = "操作失敗"
       redirect_back(fallback_location: root_path)
     end
+  end
+
+  private
+  def set_invitation
+    @invitation = Invitation.find(params[:id])
   end
 
 end
