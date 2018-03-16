8bd5e45ad9b89a43474fe45d44e675a7922ada9e
eggyy1224 <eggyy1224@gmail.com>
2018-03-16 14:01:56 +0800

    invitation測試完成

--- test/controllers/invitations_controller_test.rb
+++ test/controllers/invitations_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class InvitationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers # include devise helper methods
+  def setup
+    @admin = users(:admin)
+    @user = users(:user)
+    @user2 = users(:user2)
+    @user3 = users(:user3)
+    @user4 = users(:user4)
+    @instance = instances(:instance_teaming)
+    @invitation = invitations(:invitation1)
+  end
+
+  test "only login can see invitation_show_page" do
+    get invitation_path(@invitation)
+    assert_response :redirect
+    #被邀請的人可以看到邀請函
+    sign_in @user3
+    get invitation_path(@invitation)
+    assert_response :success
+    # 邀請人可以看到邀請函
+    sign_in @user
+    get invitation_path(@invitation)
+    assert_response :success
+  end
 end
