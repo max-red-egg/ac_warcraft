782567d326a4d4bf910cc0a29af7e796051457f6
Max <crono0720@gmail.com>
2018-03-19 14:58:16 +0800

    add invitation msg ajax

--- app/controllers/invite_msgs_controller.rb
+++ app/controllers/invite_msgs_controller.rb
@@ -1,6 +1,6 @@
 class InviteMsgsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     # 新增邀請函的留言
     invitation = Invitation.find(params[:invitation_id])
@@ -8,10 +8,16 @@ class InviteMsgsController < ApplicationController
       invite_msg = invitation.invite_msgs.create(msg_params)
       invite_msg.user = current_user
       invite_msg.save
-      redirect_to invitation_path(invitation)
+
+      @invite_msgs = invitation.invite_msgs.includes(:user)
     else
       flash[:alert] = '無法送出留言'
       redirect_back(fallback_location: root_path)
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 
