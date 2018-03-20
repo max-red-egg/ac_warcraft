de32826ac0079549e8b4367946857a12f65abd7e
spreered <spreered@gmail.com>
2018-03-20 23:19:03 +0800

    add review#submit action:  submit a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,41 +3,31 @@ class ReviewsController < ApplicationController
   def show 
   end
 
-  def create
-    # create review
 
-    # 參數說明
-    #  user: 被評論者
-    #  user_instance: 被評論者與副本的關聯table, review是依附在上面
-    #  instance: 任務副本
-    # reviews_params[:comment]: 評論內容
-    # reviews_params[:instnace_id]: 副本id
-    # params[:user_id]: 被評論者id
-    user = User.find(params[:user_id])
-    user_instance = UserInstance.find_by(instance_id: review_params[:instance_id],user_id: params[:user_id])
-    instance = Instance.find(review_params[:instance_id])
-
-    # 如果已經被current_user評論過，則跳出
-    if user.be_reviewed_from?(current_user,instance)
-      flash[:alert] = '你已經評論過 #{user.name} !'
+  def submit
+    review = Review.find(params[:id])
+    if review.reviewer != current_user
+      flash[:alert] = '::review:: 存取禁止 '
       redirect_back(fallback_location: root_path)
     end
-
-    # 如果current_user是member，而且任務已經結束，才可以新增
-    if instance.is_member?(current_user) && ( instance.state == 'complete' || instance.state == 'abort' )
-      # 新增review
-      review = user_instance.reviews.create(comment: review_params[:comment])
-      review.reviewer = current_user
+    if review.submit
+      flash[:alert] = '你已經送過評價，無法再次評論'
+      redirect_back(fallback_location: root_path)
+    end
+    if review.update!(review_params)
+      review.submit = true
       review.save
+      flash[:notice] = '成功送出評價'
       redirect_back(fallback_location: root_path)
     else
-      flash[:alert] = '無法送出評價'
+      flash[:alert] = '::review:: something wrong!'
       redirect_back(fallback_location: root_path)
     end
   end
 
+
     private
   def review_params
-    params.require(:review).permit(:comment,:instance_id)
+    params.require(:review).permit(:comment)
   end
 end
