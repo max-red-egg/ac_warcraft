e381b8ca932e197ed8f699fee7175300fa6d9ff1
Max <crono0720@gmail.com>
2018-03-20 16:38:24 +0800

    fix reverce_invitation name

--- app/helpers/invitations_helper.rb
+++ app/helpers/invitations_helper.rb
@@ -2,11 +2,13 @@ module InvitationsHelper
 
   def cross_invite_badges(invitation)
 
-    mission = invitation.instance.mission
-    missions = mission.invitations.where('invitations.user_id = ? AND invitations.invitee_id = ? AND invitations.state = ?', invitation.invitee, invitation.user ,'inviting')
+    if invitation.state == 'inviting'
+      mission = invitation.instance.mission
+      reverce_invitations = mission.invitations.where('invitations.user_id = ? AND invitations.invitee_id = ? AND invitations.state = ?', invitation.invitee, invitation.user ,'inviting')
 
-    if missions.present? && invitation.state == 'inviting'
-       content_tag(:span, '同任務互邀中' , class: "badge badge-info ml-1")
+      if reverce_invitations.present?
+         content_tag(:span, '同任務互邀中' , class: "badge badge-info ml-1")
+      end
     end
 
   end
