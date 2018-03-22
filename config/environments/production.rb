28d365e8c64d961a47182dd31db3c4d6f99ef33a
eggyy1224 <eggyy1224@gmail.com>
2018-03-22 14:38:08 +0800

    mail production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   # config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://pacific-dawn-73107.herokuapp.com' }
+  # config.action_mailer.default_url_options = { host: 'https://pacific-dawn-73107.herokuapp.com' }
   config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 
   # Ignore bad email addresses and do not raise email delivery errors.
