89041237d407e1cb2b5d8a26847d28042a1bb83b
eggyy1224 <eggyy1224@gmail.com>
2018-03-22 14:43:35 +0800

    mail pro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,11 +64,11 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ac_warcraft_#{Rails.env}"
 
   # config.action_mailer.perform_caching = false
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
-  # config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = false
 
-  # config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
   # config.action_mailer.default_url_options = { host: 'https://pacific-dawn-73107.herokuapp.com' }
