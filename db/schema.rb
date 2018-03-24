b0ece1d9c676a6af7b4468bacb9376c2c4ed51ef
spreered <spreered@gmail.com>
2018-03-24 22:18:42 +0800

    add gem : act-as-taggable-on 5.0 and setup migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_03_23_025749) do
+ActiveRecord::Schema.define(version: 2018_03_23_153846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_03_23_025749) do
     t.datetime "updated_at", null: false
     t.string "answer", default: "", null: false
     t.integer "modifier_id"
+    t.integer "xp", default: 100, null: false
   end
 
   create_table "invitations", force: :cascade do |t|
@@ -67,6 +68,7 @@ ActiveRecord::Schema.define(version: 2018_03_23_025749) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "xp", default: 100, null: false
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -77,6 +79,31 @@ ActiveRecord::Schema.define(version: 2018_03_23_025749) do
     t.integer "reviewee_id"
     t.integer "instance_id"
     t.boolean "submit", default: false, null: false
+  end
+
+  create_table "taggings", id: :serial, force: :cascade do |t|
+    t.integer "tag_id"
+    t.string "taggable_type"
+    t.integer "taggable_id"
+    t.string "tagger_type"
+    t.integer "tagger_id"
+    t.string "context", limit: 128
+    t.datetime "created_at"
+    t.index ["context"], name: "index_taggings_on_context"
+    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+    t.index ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
+    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
+    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
+    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
+    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
+  end
+
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.integer "taggings_count", default: 0
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "user_instances", force: :cascade do |t|
@@ -110,6 +137,7 @@ ActiveRecord::Schema.define(version: 2018_03_23_025749) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.boolean "available", default: true, null: false
+    t.integer "xp", default: 0, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
