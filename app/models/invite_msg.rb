da41f4a338915afd7c4a2b6e68b47bb3cd7a8f47
spreered <spreered@gmail.com>
2018-03-26 00:31:43 +0800

    add relation in invite_msg , notification, user

--- app/models/invite_msg.rb
+++ app/models/invite_msg.rb
@@ -1,4 +1,15 @@
 class InviteMsg < ApplicationRecord
   belongs_to :user
   belongs_to :invitation
+  after_create :create_notifications
+
+  def recipient
+    self.invitation.user == self.user ? self.invitation.invitee : self.invitation.user
+  end
+
+  def create_notifications
+    Notification.create(recipient: recipient, actor: self.user,
+        action: 'send_msg', notifiable: self)
+  end
+
 end
