a803d33913291376d99af4c8100d721c353d35d2
spreered <spreered@gmail.com>
2018-03-26 16:34:31 +0800

    notifications controller: add checked action , check all and read all

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,6 +6,17 @@ class NotificationsController < ApplicationController
   end
 
   def mark_as_read
+    @notification = Notification.find(params[:id])
+    @notification.update(read_at: Time.zone.now)
+    render json: {success: true}
+  end
+
+  def mark_as_checked_all
+    @notifications = Notification.where(recipient: current_user).unchecked
+    @notifications.update_all(checked_at: Time.zone.now)
+    render json: {success: true}
+  end
+  def mark_as_read_all
     @notifications = Notification.where(recipient: current_user).unread
     @notifications.update_all(read_at: Time.zone.now)
     render json: {success: true}
