55681f54e31a291288a837cd5d650bb280e674b8
spreered <spreered@gmail.com>
2018-03-26 11:38:52 +0800

    db migrate merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_03_25_081216) do
 
   # These are extensions that must be enabled in order to support this database
