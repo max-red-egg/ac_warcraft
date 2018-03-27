d26cf8a86cd692bad81a88077f3cf00916093fbf
spreered <spreered@gmail.com>
2018-03-28 00:27:41 +0800

    cancel skip auth token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  skip_before_action :verify_authenticity_token, raise: false
+  #skip_before_action :verify_authenticity_token, raise: false
   
 end
