32ba74263ee3787693e3675e6725f5333d6612b5
spreered <spreered@gmail.com>
2018-03-27 16:17:34 +0800

    notification for instance msgs

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -162,9 +162,6 @@ class Instance < ApplicationRecord
   end
 
   def actor
-    return_actor = self.members.first
-    #第一個member 通常是發起者
-
     # in_progress 有 save和沒save的狀況
     #  save: modifier
     # abort: modifier
--- app/models/instance_msg.rb
+++ app/models/instance_msg.rb
@@ -1,4 +1,33 @@
 class InstanceMsg < ApplicationRecord
+  after_commit :create_notifications
   belongs_to :instance
   belongs_to :user
+
+  def recipients
+    #   除了自己的所有人
+    return_recipients = []
+    return_recipients += self.instance.members
+    return_recipients.delete(self.user)
+    return_recipients
+  end
+
+  def actor
+    return_actor = self.user
+  end
+  def create_notifications
+    instance_temp = self.instance
+    instance_msgs = instance_temp.instance_msgs
+    if instance_msgs.count > 1
+      msg_id = instance_msgs.last(2)[0].id
+    end
+    remove_notifications = Notification.all.where(notifiable_type:"InstanceMsg",notifiable_id: msg_id)
+    if remove_notifications
+      puts "delete"
+      remove_notifications.destroy_all
+    end
+    recipients.each do |recipient|
+      Notification.create(recipient: recipient, actor: actor,
+            action: "send_msg", notifiable: self)
+    end
+  end
 end
