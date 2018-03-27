78f5989bf9b52fa9a0ad71cff9e162a48429a7f4
eggyy1224 <eggyy1224@gmail.com>
2018-03-27 10:10:08 +0800

    新增github登入功能

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   end
   namespace :admin do
   end
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "missions#my_mission"
