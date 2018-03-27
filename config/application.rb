f3c40bc0ac876e0f33a3d44029a86decb2fbffd4
Max <crono0720@gmail.com>
2018-03-27 18:41:55 +0800

    add i18n

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module AcWarcraft
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.time_zone = "Taipei"
-
+    config.i18n.default_locale = "zh-TW"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
