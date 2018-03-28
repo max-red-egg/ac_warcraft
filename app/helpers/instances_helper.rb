c913f0236c837df9febf9255e3900bccdd26b657
Max <crono0720@gmail.com>
2018-03-28 23:43:26 +0800

    add marked unreview instance

--- app/helpers/instances_helper.rb
+++ app/helpers/instances_helper.rb
@@ -83,4 +83,13 @@ module InstancesHelper
     end
   end
 
+  def review_submited?(instance,user)
+    reviews = instance.reviews.find_by(reviewer_id: user)
+    if reviews.present? && !reviews.submit?
+      '尚未給評'
+    end
+  end
+
+
+
 end
