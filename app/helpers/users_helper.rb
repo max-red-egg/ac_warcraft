7968491ea5aa320065ff2531f2bdaf4815781969
eggyy1224 <eggyy1224@gmail.com>
2018-03-29 14:11:19 +0800

    修改gravatar helper與dev.rake

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,14 +7,16 @@ module UsersHelper
   end
 
   def gravatar_for(user, class_value = '')
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: class_value)
+    image_tag(gravatar_url(user), alt: user.name, class: class_value)
   end
 
   def gravatar_url(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    if user.email =~ /.*@sample.com\z/
+      user.avatar_url
+    else
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    end
   end
 end
  
