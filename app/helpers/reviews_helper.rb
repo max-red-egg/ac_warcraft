81893c9fb9c64fd27731d2d51986f65ce689353f
spreered <spreered@gmail.com>
2018-03-29 11:49:17 +0800

    fix bug: print stars in review_helper

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,8 +1,8 @@
 module ReviewsHelper
   def print_rating_stars(rating)
     print_star = ""
-    for i in 0..5
-      if i < rating
+    for i in 1..5
+      if i <= rating
         print_star += "<i class='fa fa-star  text-gold'></i>"
       else
         print_star += "<i class='fa fa-star-o text-gold'></i>"
