20dfb62ef110201d2f783679975b8ee17529fc68
spreered <spreered@gmail.com>
2018-03-31 17:56:19 +0800

    add scope :find_by_followings in user_instance, and edit mission#dashboard action

--- app/models/user_instance.rb
+++ app/models/user_instance.rb
@@ -1,5 +1,9 @@
 class UserInstance < ApplicationRecord
   belongs_to :user
   belongs_to :instance
+  has_one :mission, through: :instance
 
+  scope :find_by_followings, ->(user){
+    where('user_id IN (?)', user.followings.map{|following| following.id})
+  } 
 end
