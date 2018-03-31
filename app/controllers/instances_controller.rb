ef3b66ab9ce0fdd5e9f2c45641fa02afea02dc04
Max <crono0720@gmail.com>
2018-03-31 12:47:54 +0800

    remove instance index

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -1,13 +1,7 @@
 class InstancesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_instance, only: [:show, :submit, :abort, :review, :cancel, :save,:edit]
-  before_action :authenticate_instance_member, only: [:show, :submit, :abort, :review, :cancel, :save, :edit]
-
-  def index
-    @instances_in_progress = current_user.instances.where(state: 'in_progress')
-    @instances_teaming = current_user.instances.where(state: 'teaming')
-    @instances_history = current_user.instances.where(state: ['complete', 'abort'])
-  end
+  before_action :set_instance, except: [:history]
+  before_action :authenticate_instance_member, except: [:history]
 
   def history
     @instances_history = current_user.instances.where(state: ['complete', 'abort'])
@@ -21,7 +15,6 @@ class InstancesController < ApplicationController
     # end
     # binding.pry
     if @instance.state == "teaming"
-
       # 篩選使用者 & 列出所有可被邀請的使用者
       @filterrific = initialize_filterrific(
             User,
@@ -80,7 +73,7 @@ class InstancesController < ApplicationController
     end
 
   end
-  
+
   def edit
     render :edit
   end
