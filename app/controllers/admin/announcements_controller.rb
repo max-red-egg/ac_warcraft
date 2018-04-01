50fa6441e85ceea4e5ce63d4968698c54b403004
spreered <spreered@gmail.com>
2018-04-02 00:43:34 +0800

    announcement : add  new create edit and update

--- app/controllers/admin/announcements_controller.rb
+++ app/controllers/admin/announcements_controller.rb
@@ -8,6 +8,43 @@ class Admin::AnnouncementsController < ApplicationController
   def show 
     @announcement = Announcement.includes(:author).find(params[:id])
   end
+
+  def new
+    @announcement = Announcement.new
+  end
+
+  def create
+    @announcement = current_user.announcements.build(announcement_params)
+    if @announcement.save
+      flash[:notice] = "announcement created!"
+      redirect_to admin_announcement_path(@announcement)
+    else
+      flash[:alert] = "something wrong!"
+      render :new
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+    if @announcement.update(announcement_params)
+      flash[:notice] = "announcement updated!"
+      redirect_to announcement_mission_path(@announcement)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def announcement_params
+
+    params.require(:announcement).permit(:title,:content)
+
+  end
+
   def set_announcement
     @announcement = Announcement.find(params[:id])
   end
