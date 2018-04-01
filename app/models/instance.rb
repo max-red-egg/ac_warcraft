ce4572b232ab77ec026123657f4709da53c31df7
spreered <spreered@gmail.com>
2018-04-01 22:59:26 +0800

    fix bug: counter cache instances_count in user

--- app/models/instance.rb
+++ app/models/instance.rb
@@ -4,8 +4,7 @@ class Instance < ApplicationRecord
   has_many :user_instances, dependent: :destroy
 
   #instance.members 表示此副本的所有成員
-  has_many :members, through: :user_instances, source: :user, counter_cache: :instances_count
-
+  has_many :members, through: :user_instances, source: :user
   #instance.mission 表示本隊的任務
   belongs_to :mission
 
--- app/models/user_instance.rb
+++ app/models/user_instance.rb
@@ -1,5 +1,5 @@
 class UserInstance < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, counter_cache: :instances_count
   belongs_to :instance
   has_one :mission, through: :instance
 
