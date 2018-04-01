f65d0dd8012ef9d53e232b734c05359c144c0313
spreered <spreered@gmail.com>
2018-04-02 00:42:30 +0800

    announcement action and view : index and show

--- app/controllers/admin/announcements_controller.rb
+++ app/controllers/admin/announcements_controller.rb
@@ -1,2 +1,14 @@
 class Admin::AnnouncementsController < ApplicationController
+  before_action :set_announcement , only:[:show,:edit,:update,:destroy]
+
+  def index
+    @announcements = Announcement.includes(:author).order(:id)
+  end
+
+  def show 
+    @announcement = Announcement.includes(:author).find(params[:id])
+  end
+  def set_announcement
+    @announcement = Announcement.find(params[:id])
+  end
 end
