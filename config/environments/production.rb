6b6a5a40653e9617407cc6ee9f77418d47937978
eggyy1224 <eggyy1224@gmail.com>
2018-04-04 22:27:45 +0800

    set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,9 +44,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://erm.zone/cable'
+  # config.action_cable.url = 'wss://erm.zone/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  # config.web_socket_server_url = "wss://erm.zone/cable"
+  config.web_socket_server_url = "wss://erm.zone/cable"
   config.action_cable.allowed_request_origins = ['https://www.erm.zone', 'http://www.erm.zone/']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
