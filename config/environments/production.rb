de9231c632748c46c48e62c94cfb8cff0872a6b0
eggyy1224 <eggyy1224@gmail.com>
2018-04-04 17:06:27 +0800

    action cable上線設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.web_socket_server_url = "wss://pacific-dawn-73107.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://pacific-dawn-73107.herokuapp.com', 'http://www.erm.zone/']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
